12f2d11d787662277332866a8547d0d82e0726e9
Rawabi990 <rawabi382@gmail.com>
2024-01-04 02:21:35 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item));
+
+console.log("أهلاً بكم في متجر عربي")
+
+console.log("أهلا بكم في أكاديمية حسوب")
